2473b31821891b99418bb87fc8f964563885822b
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-03-13 13:00:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv").config();
+// const colors = require("colors");
+// const port = process.env.PORT || 8080;
+// // middleware
+// app.use(express.json());
+// app.use(cors());
+
+
+
+// // database connection
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//     console.log(DATABASE_LOCAL);
+//     console.log("database connected");
+//   });
+
+//   app.get("/", (req, res) => {
+//     res.send("Route is running");
+//   });
+  
+//   // server
+  
+  
+//   app.listen(port, () => {
+//     console.log(`server is running at ${port}`.yellow.bold);
+//   });
+  
